140dfbfc209195952177c40fae2b024907892823
Navya Poloju <navyapoloju@nhclindia.com>
2022-12-09 18:07:41 +0530

    Added New fields in CRM Lead

--- addons/nhcl_crm_custom_module/models/crm_lead.py
+++ addons/nhcl_crm_custom_module/models/crm_lead.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 from odoo import _, api, fields, models
+from odoo.exceptions import UserError, RedirectWarning
 from odoo.tools.json import JSON
 import queue
-
+import ast
 
 class Lead(models.Model):
     _inherit = "crm.lead"
@@ -11,28 +12,78 @@ class Lead(models.Model):
     project_id = fields.Many2one('project.project', string="Project")
     source = fields.Char('Source')
     sub_source = fields.Char('Sub Source')
+    date = fields.Datetime('Date')
+    prev_user_id = fields.Many2one('res.users')
     received_on = fields.Datetime('Received On')
-    sub_source_ids = fields.One2many('sub.sources', 'crm_lead_id')
+    # no_contact_reason = fields.Selection([('ringing_no_response', 'Ringing No Response'),
+    #                                       ('phone_switched_off', 'Phone Switched off'),
+    #                                       ('invalid_number', 'Invalid Number'),
+    #                                       ('out_of_network_area', 'Out of Network Area'),
+    #                                       ('customer_disconnecting', 'Customer Disconnecting')], string="No Contact")
+    # unqualified_reason = fields.Selection([('Budget does not match', 'Budget does not match'),
+    #                                        ('location_mismatch', 'Location mismatch'),
+    #                                        ('looking_for_different_sizes', 'Looking for different sizes'),
+    #                                        ('false_enquiry', 'False Enquiry'),
+    #                                        ('possession_dates_dont_match', 'Possession dates don’t match'),
+    #                                        ('wrong_contact_number', 'Wrong contact number'),
+    #                                        ('already_a_customer', 'Already a customer'),
+    #                                        ('lost_to_competition', 'Lost to Competition'),
+    #                                        ('is_a_channel_partner', 'Is a Channel Partner'),
+    #                                        ('duplicate_customer', 'Duplicate Customer'),
+    #                                        ('needs_an_unavailable_plot_location', 'Needs an unavailable Plot Location'),
+    #                                        ('postponed_purchase', 'Postponed Purchase')
+    #                                        ], string="UnQualified Reason")
+    # lost_reason = fields.Selection([('lost_to_competition','Lost to competition'),
+    #                                 ('not_answering_or_responding', 'Not answering / responding'),
+    #                                 ('loan_issues','Loan Issues'),
+    #                                 ('budget_issues', 'Budget Issues'),
+    #                                 ('customer_already_booked', 'Customer already booked'),
+    #                                 ('customer_postponed_purchase', 'Customer Postponed Purchase'),
+    #                                 ('needs_an_unavailable_plot_location', 'Needs an unavailable plot location')
+    #                                 ],string="Lost Reason")
+    ######################### CRM Data Import Fields #################################
+    salutation = fields.Char(string='Salutation')
+    lead_status = fields.Char(string='Lead Status')
+    phone_country = fields.Char(string='Phone Country')
+    secondary_phone = fields.Char(string='Secondary Phone')
+    secondary_email = fields.Char('Secondary Email')
+    location = fields.Char(string='Location')
+    region = fields.Char(string='Region')
+    nri_type = fields.Selection([('yes', 'YES'), ('no', 'NO')], string="NRI")
+    last_sales_contacted_on = fields.Datetime(string='Last Sales Contacted On')
+    last_sales_contact_attempted_on = fields.Datetime(string='Last Sales Contact Attempted On')
+    site_visit_status = fields.Char(string='Site Visit Status')
+    last_note_by_sales = fields.Char(string='Last Note By Sales')
+    last_note_by_presales = fields.Text(string='Last Note By Presales')
+    system_tags = fields.Text(string='System Tags')
+    lead_hotness = fields.Integer('Lead Hotness')
+    last_name = fields.Char('Last Name')
+    first_name = fields.Char('First Name')
 
     def crm_lead_action(self):
         records = self.env['crm.lead'].search([('description', '!=', False)])
-        crm_leads = self.env['crm.lead'].search(['|', ('user_id', '=', False), ('team_id', '=', False)])
+        crm_leads = self.env['crm.lead'].search(['|', '|',('user_id', '=', False), ('team_id', '=', False),('type', '=', 'lead')])
         for rec in records:
             if rec.description:
                 soup = BeautifulSoup(rec.description, 'html.parser')
                 ph_lst = []
                 mail_lst = []
+                name_lst = []
                 for content in soup.contents:
                     lst = content.text.split('\n')
                     for li in lst:
-                        if 'Phone Number' in li:
+                        if 'Phone' in li:
                             ph_lst = li.split(':')
-                            if len(ph_lst) > 1:
+                            if len(ph_lst) > 1 and rec.phone != ph_lst[1]:
                                 rec.phone = ph_lst[1]
                         if 'Email' in li:
                             mail_lst = li.split(':')
-                            if len(mail_lst) > 1:
+                            if len(mail_lst) > 1 and rec.email_from != mail_lst[1]:
                                 rec.email_from = mail_lst[1]
+                        if 'Full name' in li:
+                            name_lst = li.split(':')
+                            if len(name_lst) > 1 and rec.name != name_lst[1]:
+                                rec.name = name_lst[1]
         leads = queue.Queue()
         for lead in crm_leads:
             leads.put(lead)
@@ -51,9 +102,21 @@ class Lead(models.Model):
                     leads.queue[0].write({'user_id': sale_user_ids[j]})
                     leads.get()
 
-
-class SubSource(models.Model):
-    _name = 'sub.sources'
-
-    crm_lead_id = fields.Many2one('crm.lead', string="CRM Lead", copy=False)
-    name = fields.Char('Sub Source')
+    # @api.onchange('stage_id')
+    # def _onchange_stage_id(self):
+    #     stage_id = self.stage_id
+    #     if stage_id.name == 'No Contact' or stage_id.name == 'Unqualified' or stage_id.name == 'Lost':
+    #         selection_type = False
+    #         if stage_id.name == 'No Contact' and self.no_contact_reason == False:
+    #             selection_type = 'no_contact'
+    #         elif stage_id.name == 'Unqualified' and self.unqualified_reason == False:
+    #             selection_type = 'unqualified'
+    #         elif stage_id.name == 'Lost' and self.lost_reason == False:
+    #             selection_type = 'lost'
+    #         if selection_type:
+    #             action = self.env.ref('nhcl_crm_custom_module.crm_reason_action').read()[0]
+    #             if self.ids:
+    #                 action['context'] = dict(ast.literal_eval(action.get('context')), default_crm_lead_id=self.ids[0],
+    #                                          default_selection_type=selection_type)
+    #             msg = _('Please Select Reason for stage change.')
+    #             raise RedirectWarning(msg, action, _('Select'))
